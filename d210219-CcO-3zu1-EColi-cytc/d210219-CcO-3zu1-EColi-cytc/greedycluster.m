21d098528e229c1e42b36c428b35fcb9ca0c565d
Jakob 2 Kresse2 <jakobkresse@gmail.com>
2025-01-26 11:23:35 +0100

    Simplified greedy clustering approach

---
+++ d210219-CcO-3zu1-EColi-cytc/d210219-CcO-3zu1-EColi-cytc/greedycluster.m
@@ -0,0 +1,24 @@
+function ii2 = greedycluster(sd2,ii,threshold)
+    ii_tmp=ii;
+    ii2=[];
+    %greedy clustering
+    while ~isempty(ii)
+        %remove last curve
+        ii2=[ii(end) ii2];
+        ii=ii(1:end-1);
+        iarr1=find(ii_tmp==ii2(1));
+        %find curves closer than the threshold to last curve
+        iarr2=find(sd2(iarr1,:)<threshold);
+    
+        for iX=1:length(iarr2)
+            %add similar curves to cluster
+           iarr3=find(ii==ii_tmp(iarr2(iX)));
+           if ~isempty(iarr3)
+               ii2=[ii(iarr3) ii2];
+               ii(iarr3)=NaN;
+               ii=ii(isnan(ii)==0);
+           end
+        end
+    end
+end
+
---
+++ d210219-CcO-3zu1-EColi-cytc/d210219-CcO-3zu1-EColi-cytc/pairdistances.m
@@ -0,0 +1,13 @@
+function sd = pairdistances(tmpX_all)
+ %calculate pairwise distances
+    sd=0*ones(size(tmpX_all,2),size(tmpX_all,2));
+    for iX=1:size(tmpX_all,2)
+        for iY=iX+1:size(tmpX_all,2)
+            tmptmp=tmpX_all(:,iX).*tmpX_all(:,iY);
+            iarr=find(isnan(tmptmp)==0);
+            sd(iX,iY)=mean(power(tmpX_all(iarr,iX)-tmpX_all(iarr,iY),2));
+            sd(iY,iX)=sd(iX,iY);
+        end
+    end
+end
+
